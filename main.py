9ec0b85fcfb32ee2580e0206dbb4f406830057c0
Nokky07 <kieranmcm2007@gmail.com>
2024-01-26 22:47:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import os 
+
+
+#Clear Function
+def clear():
+  if os.name == "nt":
+    os.system("cls")
+  else:
+    os.system("clear")
+
+print("           ")
+time.sleep(0.1)
+clear()
+print("|         |")
+time.sleep(0.1)
+clear()
+print("||       ||")
+time.sleep(0.1)
+clear()
+print("|||     |||")
+time.sleep(0.1)
+clear()
+print("||||   ||||")
+time.sleep(0.1)
+clear()
+print("||||| |||||")
+time.sleep(0.1)
+clear()
+print("|||||||||||")
+time.sleep(0.1)
+clear()
+print("|||||F|||||")
+time.sleep(0.1)
+clear()
+print("|||| Fi||||")
+time.sleep(0.1)
+clear()
+print("|||s Fig|||")
+time.sleep(0.1)
+clear()
+print("||ss Figh||")
+time.sleep(0.1)
+clear()
+print("|oss Fight|")
+time.sleep(0.1)
+clear()
+print("Boss Fight")
